9e6e073b30588b4dcf277fb4782fc5f02c9bb864
bt22103030pratimbiswas <120629458+bt22103030pratimbiswas@users.noreply.github.com>
2022-12-22 14:37:49 +0530

    Add files via upload

---
+++ Assignment 2 question 1.py
@@ -0,0 +1,9 @@
+str="python is a case sensitive language"
+print("length of string =",len(str))
+print(str[::-1])
+y=str[10:26:1]
+print(y)
+replacing=str.replace("a case sensitive","object oriented")
+print(replacing)
+print(str.index("a"))
+print(str.replace(" ",""))
